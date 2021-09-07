1e423b25c1d69091967c4dc65de435b5a1d17828
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2021-09-07 08:12:59 +0200

    :sparkles: td2

---
+++ src/td1/FileSansDecallageGeneric.java
@@ -0,0 +1,137 @@
+package td1;
+
+import java.util.Scanner;
+
+public class FileSansDecallageGeneric<A> implements FileGeneric<A> {
+  private static class Node<T> {
+    private T element;
+    private Node<T> next;
+
+    public Node(T element) {
+      this.element = element;
+      next = null;
+    }
+
+    public int length() {
+      if (next == null) {
+        return 1;
+      } else {
+        return 1 + next.length();
+      }
+    }
+
+    public String toString() {
+      if (next == null) {
+        return String.valueOf(element);
+      } else {
+        return element + ", " + next;
+      }
+    }
+
+    public void add(T element) {
+      if (next == null) {
+        next = new Node<T>(element);
+      } else {
+        next.add(element);
+      }
+    }
+
+    public T getElement() {
+      return element;
+    }
+
+    public void setElement(T element) {
+      this.element = element;
+    }
+
+    public Node<T> getNext() {
+      return next;
+    }
+
+    public void setNext(Node<T> next) {
+      this.next = next;
+    }
+  }
+
+
+  private Node<A> content;
+
+  public static void main(String[] args) {
+  Scanner scanner = new Scanner(System.in);
+  FileGeneric<Float> file = new FileSansDecallageGeneric(); // Seul changement ici
+    while (!file.estPleine()) {
+    System.out.print("Veuillez saisir un nombre (-1 pour arreter): ");
+    float userValue = scanner.nextFloat();
+    if (userValue < 0) break;
+    file.ajouter(userValue);
+  }
+    scanner.close();
+    System.out.println(file);
+}
+
+  public FileSansDecallageGeneric() {
+    content = null;
+  }
+
+  /**
+   * @return vrai si la file est vide
+   * @implNote O(1)
+   */
+  public boolean estVide() {
+    return this.content == null;
+  }
+
+  /**
+   * @return vrai si la file est pleine
+   * @implNote O(1)
+   */
+  public boolean estPleine() {
+    return false;
+  }
+
+  /**
+   * @return le nombre d’elements presents dans la file
+   * @implNote O(1)
+   */
+  public int getTaille() {
+    return content == null ? 0 : content.length();
+  }
+
+  /**
+   * @param x : valeur a ajouter en fin de file
+   * @implNote O(1)
+   */
+  public void ajouter(A x) {
+    if (content == null) {
+      content = new Node<>(x);
+    } else {
+      content.add(x);
+    }
+
+  }
+
+  /**
+   * supprimer l’element de tete de file .
+   * @implNote O(N)
+   */
+  public void supprimer() {
+    if (content != null) {
+      content = content.getNext();
+    }
+  }
+
+  /**
+   * @return valeur de l’element en tete de file
+   * @implNote O(1)
+   */
+  public A getTete() {
+    return content.element;
+  }
+
+  /**
+   * @implNote O(N)
+   */
+  public String toString() {
+    return content == null ? "" : content.toString();
+  }
+}
---
+++ src/td1/FileTableau.java
@@ -0,0 +1,84 @@
+package td1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FileTableau implements File {
+  private final float[] content;
+  private int amount;
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    File file = new FileTableau(5);
+    while (!file.estPleine()) {
+      System.out.print("Veuillez saisir un nombre (-1 pour arreter): ");
+      float userValue = scanner.nextFloat();
+      if (userValue < 0) break;
+      file.ajouter(userValue);
+    }
+    scanner.close();
+    System.out.println(file);
+  }
+
+  public FileTableau(int capacity) {
+    content = new float[capacity];
+    amount = 0;
+  }
+
+  /**
+   * @return vrai si la file est vide
+   * @implNote O(1)
+   */
+  public boolean estVide() {
+    return amount == 0;
+  }
+
+  /**
+   * @return vrai si la file est pleine
+   * @implNote O(1)
+   */
+  public boolean estPleine() {
+    return amount == content.length - 1;
+  }
+
+  /**
+   * @return le nombre d’elements presents dans la file
+   * @implNote O(1)
+   */
+  public int getTaille() {
+    return amount;
+  }
+
+  /**
+   * @param x : valeur a ajouter en fin de file
+   * @implNote O(1)
+   */
+  public void ajouter(float x) {
+    if (this.estPleine()) return;
+    content[++amount - 1] = x;
+  }
+
+  /**
+   * supprimer l’element de tete de file .
+   * @implNote O(N)
+   */
+  public void supprimer() {
+    System.arraycopy(content, 1, content, 0, content.length);
+    amount--;
+  }
+
+  /**
+   * @return valeur de l’element en tete de file
+   * @implNote O(1)
+   */
+  public float getTete() {
+    return content[0];
+  }
+
+  /**
+   * @implNote O(N)
+   */
+  public String toString() {
+    return Arrays.toString(Arrays.copyOfRange(content, 0, amount));
+  }
+}
