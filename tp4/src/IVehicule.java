3e448230bd8674d59d9376b3697e941eec3511b6
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-09-26 17:06:21 +0200

    :bento: added base file

---
+++ tp4/src/IVehicule.java
@@ -0,0 +1,20 @@
+/**
+ * Modèle de véhicule
+ */
+public interface IVehicule
+{
+  // déterminer la longueur d'un véhicule
+  public int getLongueur();
+
+  // déterminer le nombre de passagers
+  public int getPassagers();
+
+  // connaître l'immatriculation
+  public String getImmatriculation();
+
+  // représentation affichable
+  public String toString();
+
+  // calculer le tarif du véhicule
+  public float calculerTarif();
+}
